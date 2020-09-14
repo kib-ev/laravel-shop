998ad66a7ee56db0ae5cd5e0d69a92b61ff074c1
Yauhen Kib <info@kib-ev.by>
2020-09-14 17:59:49 +0300

    product category model added

---
+++ database/migrations/2020_09_14_101940_create_product_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->id('parent_id')->default('0');
+            $table->string('name', 255);
+            $table->string('image_path', 255)->default('');
+            $table->text('excerpt');
+            $table->text('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
