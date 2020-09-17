ac72b2237d086efd6d15c9e349af38b29a2fc93c
kib-ev <zhen.kib@gmail.com>
2020-09-17 09:45:27 +0300

    vagrant file added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use App\Models\ProductCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder {
     /**
@@ -13,7 +15,16 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        // User::factory(10)->create();
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory(5)->create();
         ProductCategory::factory(20)->create();
         Product::factory(125)->create();
     }
