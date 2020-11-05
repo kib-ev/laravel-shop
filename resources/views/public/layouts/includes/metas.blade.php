b8007dcd3fa91c6731b17105bbe88f0d1450f401
kib-ev <zhen.kib@gmail.com>
2020-11-05 03:29:18 +0300

    added creating pages from url

--- resources/views/public/layouts/includes/metas.blade.php
+++ resources/views/public/layouts/includes/metas.blade.php
@@ -1,16 +1,30 @@
 <div class="container">
+
     <form method="post" action="{{ meta('id') ? route('admin.metas.update', meta('id')) : route('admin.metas.store') }}">
         @csrf
-        @method('patch')
 
+        @if(meta('id'))
+            @method('patch')
+        @else
+            @method('post')
+        @endif
+
+        <p>Meta {{ meta('id') }}</p>
         <table class="table table-bordered">
             <tr>
                 <td>uri</td>
-                <td><input type="text" name="uri" value="{{ meta()->uri  }}" disabled></td>
+                <td>
+                    <input type="hidden" name="uri" value="{{ meta()->uri  }}">
+                    <input type="text" value="{{ meta()->uri  }}" disabled>
+                </td>
+
             </tr>
             <tr>
                 <td>lang</td>
-                <td><input type="text" name="lang" value="{{ meta()->lang  }}" disabled></td>
+                <td>
+                    <input type="hidden" name="lang" value="{{ meta()->lang  }}">
+                    <input type="text" value="{{ meta()->lang  }}" disabled>
+                </td>
             </tr>
             <tr>
                 <td>title</td>
